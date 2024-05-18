d2e57ca193cf18bb8aa7286de50f339541efc53e
Andrey Kaiky Reis Ferreira <98493968+JovemKA@users.noreply.github.com>
2024-05-18 21:04:57 +0000

    Atualizando app.js

--- src/app.js
+++ src/app.js
@@ -1,30 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { config } = require('dotenv');
-const { sequelize } = require('../models');
-const errorHandler = require('../middlewares/errorHandler');
-const router = require('../routes/router');
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+import errorHandler from './middlewares/errorHandler';
+import router from './routes/router';
 
-config();
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set');
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
+
 app.use(bodyParser.json());
-
-// Importar rotas
-const userRoutes = require('../routes/users');
-const directorRoutes = require('../routes/directors');
-
-// Usar rotas
-app.use('/users', userRoutes);
-app.use('/directors', directorRoutes);
-
-// Middleware de tratamento de erros
+app.use('/', router);
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 sequelize.authenticate()
