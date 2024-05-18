154c67de72cae129b23ab4d120b0b7d3f76c9b99
Andrey Kaiky Reis Ferreira <98493968+JovemKA@users.noreply.github.com>
2024-05-18 20:32:30 +0000

    Finalizando controllers e routes e projeto funcionando

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { config } = require('dotenv');
+const { sequelize } = require('./models');
+const errorHandler = require('./middlewares/errorHandler');
+const router = require('./routes/router');
+
+config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// Importar rotas
+const userRoutes = require('./routes/users');
+const directorRoutes = require('./routes/directors');
+
+// Usar rotas
+app.use('/users', userRoutes);
+app.use('/directors', directorRoutes);
+
+// Middleware de tratamento de erros
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
--- config/config.js
+++ config/config.js
@@ -1,10 +1,27 @@
+// config/config.js
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    dialectOptions: { ssl: { require: true }, quoteIdentifiers: false },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // ou true, dependendo das configurações do Neon
+      }
+    }
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // ou true, dependendo das configurações do Neon
+      }
+    }
   }
 };
